52a45c092180afb2c576d4307ecd6f96d926085f
Emerson Zapata <emerson.zapatarivas@gmail.com>
2022-10-13 08:49:28 -0600

    first commit

---
+++ JS/index.js
@@ -0,0 +1,15 @@
+const prev = document.querySelector('.prev')
+const next = document.querySelector('.next')
+const slider = document.querySelector('.slider')
+
+prev.addEventListener('click', ()=>{
+    slider.scrollLeft -= 300
+})
+
+next.addEventListener('click', ()=>{
+    slider.scrollLeft += 300
+})
+
+function toggleMobileMenu(menu){
+  menu.classList.toggle('open');
+}
